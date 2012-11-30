337b70f34e7f92a03f653e754e61aa4d9380f664
poorni.poorni <poorni.poorni@gmail.com>
2012-11-30 05:38:50 +0000

    Progressive download of byte stream from media url is in progress. Issue: It picks the radio url as video??

--- NPR Streaming/assets/www/BufferAudioPlugin.js
+++ NPR Streaming/assets/www/BufferAudioPlugin.js
@@ -1,6 +1,18 @@
 var BufferAudioPlugin = {
-    callNativeFunction: function (success, fail, resultType) {
-    	 
-    	return cordova.exec(success, fail, "com.streaming.StreamAudio", "playAudio", [resultType]);
-    }
+    
+	bufferRadioFunc: function (success, fail, resultType) {	 
+		       
+	   	return cordova.exec(success, fail, "com.streaming.StreamAudio", "bufferRadio", [resultType]);
+	},
+		    
+	playRadioFunc: function (success, fail, resultType) {	 
+       
+    	return cordova.exec(success, fail, "com.streaming.StreamAudio", "playRadio", [resultType]);
+    },
+
+	stopRadioFunc: function (success, fail, resultType) {	 
+	   
+		return cordova.exec(success, fail, "com.streaming.StreamAudio", "stopRadio", [resultType]);
+	}
+    
 };
